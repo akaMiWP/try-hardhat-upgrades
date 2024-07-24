8c0a1e910447b0badcb42033a98fa389f1e12486
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-24 20:44:33 +0700

    Added BoxV2 contract and scripts

--- scripts/create-box.js
+++ scripts/create-box.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const Box = await ethers.getContractFactory("Box");
-  const box = await upgrades.deployProxy(Box, [42]);
+  const box = await upgrades.deployProxy(Box, [42], { initializer: "store" });
   await box.waitForDeployment();
   console.log("Box deployed to:", await box.getAddress());
 }
